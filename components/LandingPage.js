b0f676255893001fa9762a907866c757b9636462
oreoluwade <oreoluwade@gmail.com>
2017-11-13 12:34:36 +0100

    create styles directory

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Text, View, TouchableHighlight } from 'react-native';
-import { landingStyles as styles } from '../src/styles';
+import { landingStyles as styles } from '../styles';
 
 export default class LandingPage extends React.Component {
   static navigationOptions = {
---
+++ styles/index.js
@@ -0,0 +1,5 @@
+export {
+  landingStyles,
+  signupStyles,
+  textfieldStyles
+} from './styles';
