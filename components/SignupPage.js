e0eba367607bf0e4e5a828dca1c13d8ad8414db1
omorilewa <mariam3105@gmail.com>
2017-11-13 19:57:59 +0100

    add apollo client

--- components/SignupPage.js
+++ components/SignupPage.js
@@ -5,24 +5,37 @@ import {
   Image,
   TouchableHighlight
 } from 'react-native';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
 import TextField from './TextField';
 import { signupStyles as styles } from '../styles';
 import SignUpImage from '../assets/sign.png';
 
-export default class SignupPage extends React.Component {
+class SignupPage extends React.Component {
   static navigationOptions = {
     title: 'Sign Up',
   };
+
+  static propTypes = {
+    signUp: PropTypes.func
+  }
   constructor() {
     super();
     this.state = {
-      FirstName: '',
-      Lastname: '',
-      Email: '',
-      Password: '',
-      ConfirmPassword: '',
-      PhoneNo: ''
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      passwordConfirm: '',
+      phoneNum: ''
     };
+  }
+
+  submit = () => {
+    this.props.signUp(this.state).then((response) => {
+      console.log(response.data);
+    });
   }
 
   render() {
@@ -34,40 +47,41 @@ export default class SignupPage extends React.Component {
         />
         <TextField
           placeholder='Firstname'
-          value={this.state.FullName}
-          onChangeText={FirstName => this.setState({ FirstName })}
+          value={this.state.firstName}
+          onChangeText={firstName => this.setState({ firstName })}
         />
         <TextField
           placeholder='Lastname'
-          value={this.state.FullName}
-          onChangeText={LastName => this.setState({ LastName })}
+          value={this.state.lastName}
+          onChangeText={lastName => this.setState({ lastName })}
         />
         <TextField
           placeholder='E-mail'
-          value={this.state.Email}
-          onChangeText={Email => this.setState({ Email })}
+          value={this.state.email}
+          onChangeText={email => this.setState({ email })}
           keyboardType='email-address'
         />
         <TextField
           placeholder='Password'
           secureTextEntry
-          value={this.state.Password}
-          onChangeText={Password => this.setState({ Password })}
+          value={this.state.password}
+          onChangeText={password => this.setState({ password })}
         />
         <TextField
           placeholder='Confirm Password'
           secureTextEntry
-          value={this.state.ConfirmPassword}
-          onChangeText={ConfirmPassword => this.setState({ ConfirmPassword })}
+          value={this.state.passwordConfirm}
+          onChangeText={passwordConfirm => this.setState({ passwordConfirm })}
         />
         <TextField
           placeholder='+234 802 9040 852'
-          value={this.state.PhoneNo}
-          onChangeText={PhoneNo => this.setState({ PhoneNo })}
+          value={this.state.phoneNum}
+          onChangeText={phoneNum => this.setState({ phoneNum })}
           keyboardType='phone-pad'
         />
         <TouchableHighlight
           style={styles.buttonStyle}
+          onPress={this.submit}
         >
           <Text style={{ color: '#fff' }}>
             SUBMIT DETAILS
@@ -78,3 +92,48 @@ export default class SignupPage extends React.Component {
   }
 }
 
+const SignUpMutation = gql`
+  mutation createUser(
+    $firstName: String!,
+    $lastName : String!,
+    $email: String!,
+    $password: String!,
+    $passwordConfirm: String!,
+    $phoneNum: String!
+  ){
+    createUser(
+      firstName: $firstName,
+      lastName: $lastName,
+      email: $email,
+      password: $password,
+      passwordConfirm: $passwordConfirm,
+      phoneNum: $phoneNum) {
+        firstName
+        email
+    }
+  }
+`;
+
+const SignUpWithData = graphql(SignUpMutation, {
+  props: ({ mutate }) => ({
+    signUp: ({
+      firstName,
+      lastName,
+      email,
+      password,
+      passwordConfirm,
+      phoneNum
+    }) => mutate({
+      variables: {
+        firstName,
+        lastName,
+        email,
+        password,
+        passwordConfirm,
+        phoneNum
+      }
+    }),
+  }),
+})(SignupPage);
+
+export default SignUpWithData;
