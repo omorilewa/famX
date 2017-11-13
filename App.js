8572fb5f6b8b821efcc502dc23e6fdc799e5fc59
omorilewa <mariam3105@gmail.com>
2017-11-13 16:55:48 +0100

    add apollo client

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { addGraphQLSubscriptions } from 'add-graphql-subscriptions';
 import LandingPage from './components/LandingPage';
 import SignUpPage from './components/SignupPage';
+
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj9y5vwd52g6c0136n0xaz7tn', {
+  reconnect: true
+});
+
+const link = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cj9y5vwd52g6c0136n0xaz7tn' });
+const linkWithSubscriptions = addGraphQLSubscriptions(
+  link,
+  wsClient
+);
+
+const client = new ApolloClient({
+  link: linkWithSubscriptions,
+  cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
+});
 
 const Nav = StackNavigator({
   LandingPage: { screen: LandingPage },
   SignUpPage: { screen: SignUpPage }
-  }
-)
+});
 
 export default class App extends React.Component {
-
   render() {
     return (
-    <Nav />
+      <ApolloProvider client={client}>
+         <Nav />
+      </ApolloProvider>
     );
   }
 }
