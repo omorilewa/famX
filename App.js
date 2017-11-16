51906f3c918913279d12adef3e527c8c9aaa7cd5
omorilewa <mariam3105@gmail.com>
2017-11-16 12:41:51 +0100

    refactor app setup

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { combineReducers, createStore } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import { StackNavigator } from 'react-navigation';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-client';
@@ -6,6 +8,7 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { SubscriptionClient } from 'subscriptions-transport-ws';
 import { addGraphQLSubscriptions } from 'add-graphql-subscriptions';
+import { Provider } from 'react-redux';
 import LandingPage from './components/LandingPage';
 import SignUpPage from './components/SignupPage';
 
@@ -13,9 +16,13 @@ const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj9y5
   reconnect: true
 });
 
-const link = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cj9y5vwd52g6c0136n0xaz7tn' });
+const uri = 'https://api.graph.cool/simple/v1/cj9y5vwd52g6c0136n0xaz7tn';
+
+const httpLink =
+new HttpLink({ uri });
+
 const linkWithSubscriptions = addGraphQLSubscriptions(
-  link,
+  httpLink,
   wsClient
 );
 
@@ -23,6 +30,10 @@ const client = new ApolloClient({
   link: linkWithSubscriptions,
   cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
 });
+
+const store = createStore(combineReducers({
+  form: formReducer
+}));
 
 const Nav = StackNavigator({
   LandingPage: { screen: LandingPage },
@@ -32,9 +43,11 @@ const Nav = StackNavigator({
 export default class App extends React.Component {
   render() {
     return (
-      <ApolloProvider client={client}>
-         <Nav />
-      </ApolloProvider>
+      <Provider store={store}>
+        <ApolloProvider client={client}>
+            <Nav />
+        </ApolloProvider>
+      </Provider>
     );
   }
 }
