0bb3d15d0181e3c7dafc11db43d16ab42c31e19a
omorilewa <mariam3105@gmail.com>
2017-11-21 20:19:27 +0100

    update index file

--- components/index.js
+++ components/index.js
@@ -1,3 +1,5 @@
 export { default as SignupForm } from './SignupPage';
 export { default as LoginForm } from './LoginPage';
 export { default as LandingPage } from './LandingPage';
+export { default as CreateFamilyPage } from './CreateFamilyPage';
+export { default as InputField } from './InputField';
