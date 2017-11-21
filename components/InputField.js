f27425f0aa65f75dad5e1f54c77a357977c14157
omorilewa <mariam3105@gmail.com>
2017-11-21 20:19:47 +0100

    add input field to render field

---
+++ components/InputField.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormInput } from 'react-native-elements';
+import { textfieldStyles as styles } from '../styles';
+
+const InputField = () =>
+<FormInput containerStyle={styles.containerStyle}/>;
+
+
+export default InputField;
