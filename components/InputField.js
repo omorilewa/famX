a3fb5422254e357184476b760148c9c51794c28f
oreoluwade <oreoluwade@gmail.com>
2017-12-07 10:34:09 +0100

    update InputField component with additional props

--- components/InputField.js
+++ components/InputField.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import { FormInput } from 'react-native-elements';
+import PropTypes from 'prop-types';
 import { createFamilyStyles as styles } from '../styles';
 
-const InputField = () =>
-<FormInput containerStyle={styles.inputStyleFam}/>;
+const InputField = (props) => {
+  const {
+    placeholder,
+    input: { onChange, ...restInput }
+  } = props;
+  return (
+    <FormInput
+      containerStyle={styles.inputStyleFam}
+      placeholder={placeholder}
+      {...restInput}
+      onChangeText={onChange}
+    />
+  );
+};
+
+InputField.propTypes = {
+  placeholder: PropTypes.string,
+  input: PropTypes.object
+};
 
 
 export default InputField;
